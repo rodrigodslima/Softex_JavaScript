15b60f01bcc648667d6ea658795f40479f588a3e
Rodrigo Daniel <119114888+rodrigodslima@users.noreply.github.com>
2025-09-05 22:32:10 -0300

    Create aula7.js

---
+++ aulas/aula7/aula7.js
@@ -0,0 +1,8 @@
+// Criando um array com 4 times
+let timesDeFutebol = ["Flamengo", "Corinthians", "Palmeiras", "São Paulo"];
+
+// Imprimindo o primeiro time (índice 0)
+console.log("Primeiro time:", timesDeFutebol[0]);
+
+// Imprimindo o último time (usando length - 1)
+console.log("Último time:", timesDeFutebol[timesDeFutebol.length - 1]);
