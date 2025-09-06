90e2837d567f29accf1baf7f86697955b71aa21a
Rodrigo Daniel <119114888+rodrigodslima@users.noreply.github.com>
2025-09-05 22:30:01 -0300

    Create aula7-1.js

---
+++ aulas/aula6/aula7/aula7-1.js
@@ -0,0 +1,10 @@
+// Criando um array vazio
+let minhaListaDeTarefas = [];
+
+// Adicionando tarefas com .push()
+minhaListaDeTarefas.push("Comprar pão");
+minhaListaDeTarefas.push("Estudar JavaScript");
+minhaListaDeTarefas.push("Fazer exercícios");
+
+// Imprimindo o array completo
+console.log(minhaListaDeTarefas);
